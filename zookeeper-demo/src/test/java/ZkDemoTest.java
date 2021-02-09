bbc2da0bffc2ba6b2d393a4c3c9b385fe4249011
zb666 <1360799362@qq.com>
2021-02-09 11:04:43 +0800

    保存临时zk节点

---
+++ zookeeper-demo/src/test/java/ZkDemoTest.java
@@ -0,0 +1,29 @@
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ZkDemoTest {
+
+    public static void main(String[] args) throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString("47.98.181.113:2181")
+                .retryPolicy(retryPolicy)
+                .sessionTimeoutMs(6000)
+                .connectionTimeoutMs(6000)
+                .build();
+
+        client.create()
+                .creatingParentContainersIfNeeded() // 自动递归创建父节点
+                .withMode(CreateMode.PERSISTENT)
+                .forPath("/node_curator","xbq".getBytes());
+    }
+
+
+
+}
